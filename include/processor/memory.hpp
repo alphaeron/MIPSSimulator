1708df895d4d9f24ccfed800e7e88029a2377cb4
John Matty <jcmjohnmatty@me.com>
2015-11-21 15:29:23 -0500

    Added initial Memory implementation.

--- include/processor/memory.hpp
+++ include/processor/memory.hpp
@@ -4,8 +4,53 @@
 class Memory
 {
 public:
+	// CONSTRUCTORS
+
+	/**
+	 * @brief Construct main memory given a size for it.
+	 *
+	 * @param[in] mem_size The size of main memory.
+	 */
+	Memory (size_t mem_size);
+
+	// DESTRUCTORS
+
+	/**
+	 * @brief Destructor.
+	 */
+	~Memory ();
+
+	// OPERATORS
+
+	/**
+	 * @brief Index memory as you normally would.
+	 *
+	 * @param[in] address The "address" to use as an index into this
+	 *            <code>Memory</code>.
+	 *
+	 * @return The value of this <code>Memory</code> at <code>address</code>.
+	 */
+	unsigned
+	operator[] (unsigned address) const;
+
+	/**
+	 * @brief Index memory as you normally would.
+	 *
+	 * @param[in] address The "address" to use as an index into this
+	 *            <code>Memory</code>.
+	 *
+	 * @return The value of this <code>Memory</code> at <code>address</code>.
+	 */
+	unsigned&
+	operator[] (unsigned address);
 
 private:
+	// VARIABLES
+
+	/** The size of data. */
+	size_t m_size;
+	/** The data that is stored in this <code>Memory</code>. */
+	unsigned* data;
 };
 
 #endif // __MIPSSIMULATOR_PROCESSOR_MEMORY_HPP_
--- lib/processor/memory.cpp
+++ lib/processor/memory.cpp
@@ -1,1 +1,26 @@
 #include <processor/memory.hpp>
+
+Memory::Memory (size_t mem_size)
+	: m_size (mem_size)
+{
+	data = new unsigned[mem_size];
+}
+
+Memory::~Memory ()
+{
+	delete [] data;
+}
+
+unsigned
+Memory::operator[] (unsigned address) const
+{
+	/// @todo Bounds checking and raise exception if outsize bounds.
+	return data[address];
+}
+
+unsigned&
+Memory::operator[] (unsigned address)
+{
+	/// @todo Bounds checking and raise exception if outsize bounds.
+	return data[address];
+}
