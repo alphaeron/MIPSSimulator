825c7b5fea3573ed01ea0d5a93927b511259a0a8
John Matty <jcmjohnmatty@me.com>
2015-11-22 11:17:58 -0500

    Added basic OS construction.

--- include/os/os.hpp
+++ include/os/os.hpp
@@ -1,11 +1,38 @@
 #ifndef __MIPSSIMULATOR_OS_OS_HPP_
 #define  __MIPSSIMULATOR_OS_OS_HPP_
 
+#include <processor/clock.hpp>
+#include <processor/memory.hpp>
+#include <processor/processor.hpp>
+
 class OS
 {
 public:
+	// CONSTRUCTORS
+
+	/**
+	 * @brief Construct the OS.
+	 *
+	 * @param[in] clock     The clock assosiated with this system.
+	 * @param[in] processor The processor this <code>OS</code> is running on.
+	 * @param[in] memory    The memory for this system
+	 */
+	OS (Clock& clock, MIPSProcessor& processor, Memory& memory);
+
+	/**
+	 * Destructor.
+	 */
+	~OS ();
 
 private:
+	// VARIABLES
+
+	/** THe clock assosiated with this system. */
+	Clock& m_clock;
+	/** The processor this <code>OS</code> is running on. */
+	MIPSProcessor& m_processor;
+	/** THe memory for this system. */
+	Clock& m_memory;
 };
 
 #endif //  __MIPSSIMULATOR_OS_OS_HPP_
--- lib/os/os.cpp
+++ lib/os/os.cpp
@@ -1,1 +1,10 @@
 #include <os/os.hpp>
+
+inline
+OS::OS (Clock& clock, MIPSProcessor& processor, Memory& memory)
+	: m_clock (clock), m_processor (processor), m_memory (memory)
+{ }
+
+inline
+OS::~OS ()
+{ }
